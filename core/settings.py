cca777653d8445e77f0602b392c6d5e14a81d3af
fraynilson2003 <fraynilson2003@gmail.com>
2023-08-15 12:29:13 -0500

    url back

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "34.95.253.218", os.getenv("APP_BACKEND")]
+ALLOWED_HOSTS = ["localhost", "https://ai-pedro.onrender.com", os.getenv("APP_BACKEND")]
 
 
 # Application definition

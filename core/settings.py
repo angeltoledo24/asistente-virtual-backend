2d0878da8c41cfc5bdac50b8a8190f504329472f
fraynilson2003 <fraynilson2003@gmail.com>
2023-08-15 11:24:45 -0500

    m

--- core/settings.py
+++ core/settings.py
@@ -12,9 +12,9 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", os.getenv('APP_BACKEND')]
+ALLOWED_HOSTS = ["localhost", "34.125.48.50", os.getenv("APP_BACKEND")]
 
 
 # Application definition
